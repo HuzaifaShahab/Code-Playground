9e1df457cb750fa62066b53c34ff20f111fe0c02
HUZAIFA SHAHAB <huzaifarifatullah@gmail.com>
2023-07-26 20:27:52 +0500

    Create Element_search_multiArray.java
    In this code basically we are practicing to figure out elements that are not common.

---
+++ Element_search_multiArray.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Element_search_multiArray {
+    public static void main(String[] args) {
+
+                final int row = 2 ;
+                final int col = 5 ;
+
+                int [][] multiArray = new int[row][col] ;
+
+                Scanner inpNumber = new Scanner(System.in) ;     // User will input values for each index.
+                for (int i=0 ; i< multiArray.length ; i++){
+                    for (int j=0 ; j<multiArray[i].length ; j++){
+                        System.out.print("Input element at index ["+i+"]["+j+"] : ");
+                        multiArray[i][j] = inpNumber.nextInt() ;
+                        System.out.println();
+                    }
+                }
+                // Elements of row-1 that are not in row-2
+                System.out.println("Elements of row-1 that are not in row-2");
+                for (int i = 0 ; i<multiArray[0].length ; i++){
+                    int element = multiArray[0][i] ;              // 1st Element
+                    boolean mapElement = true ;
+
+                    for (int j=0 ; j<multiArray[1].length ; j++){
+                        if (element == multiArray[1][j]){
+                            mapElement = false ;
+                            break ;
+                        }
+                    }
+                    if (mapElement){   // Element not found in row-2
+                        System.out.print(element+" ");
+                    }
+                }
+                System.out.println();
+                System.out.println("Elements of row-2 that are not in row-1");
+                for (int i=0 ; i<multiArray[1].length ; i++){
+                    int element = multiArray[1][i] ; // 2nd-row 1st element.
+                    boolean mapEle = true ;
+                    for (int j =0 ; j<multiArray[0].length ; j++){
+                        if (element == multiArray[0][j]){
+                            mapEle = false ;
+                            break;
+                        }
+                    }
+                    if (mapEle){ // Element not found in row-1
+                        System.out.print(element+" ");
+                    }
+                }
+            }
+        }
