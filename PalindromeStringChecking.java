57c42c00b9352b244783905c3b583220c29a5113
HUZAIFA SHAHAB <huzaifarifatullah@gmail.com>
2023-08-04 15:09:53 +0500

    Update PalindromeStringChecking.java

--- PalindromeStringChecking.java
+++ PalindromeStringChecking.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
 public class PalindromeStringChecking {
-    public static void checkPalindrome (String palin){
+    public static boolean checkPalindrome (String palin){
        for (int i=0 ; i<palin.length()/2 ; i++){
            if (palin.charAt(i) != palin.charAt(palin.length()-1-i)){
-               System.out.println("Given Text Is Not A Palindrome");
+              return false ;
            }
        }
-
+            return true ;
     }
     public static void main(String[] args) {
         String palin = null ;
@@ -16,7 +16,10 @@ public class PalindromeStringChecking {
         System.out.println("Give me any word");
          palin = inputPalin.next() ;
 
-         checkPalindrome(palin);
+         boolean flag =checkPalindrome(palin);
+         
+         if (flag) System.out.println("Given Text Is A Palindrome");
+         else System.out.println("Given Text Is Not A Palindrome");
 
     }
 }
