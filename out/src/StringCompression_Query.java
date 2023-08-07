0272071d14c4f0954c62e4dd6618c51b69b4d8be
HUZAIFA SHAHAB <huzaifarifatullah@gmail.com>
2023-08-07 15:11:28 +0500

    Update StringCompression_Query.java

--- out/src/StringCompression_Query.java
+++ out/src/StringCompression_Query.java
@@ -1,20 +1,20 @@
 public class StringCompression_Query {
     public static void compressString(String str){
-        StringBuilder newStr = new StringBuilder("") ;
+        StringBuilder newStr = new StringBuilder("") ; // Creating newStr object
 
-        for (int i=0; i<str.length(); i++){
-            Integer count = 1 ;
-            while (i<str.length()-1 && str.charAt(i)==str.charAt(i+1)){
+        for (int i=0; i<str.length(); i++){ // Picking each element one-by-one
+            Integer count = 1 ;      // Using Integer Class for string purpose
+            while (i<str.length()-1 && str.charAt(i)==str.charAt(i+1)){  // i<n-1 b/c comparing i with next index.
                 count++ ;
                 i++;
             }
-            newStr.append(str.charAt(i)) ;
-            if (count > 1) newStr.append(count) ;
+            newStr.append(str.charAt(i)) ;   // Appending letter.
+            if (count > 1) newStr.append(count) ; // Appending count to String also.
         }
-        System.out.println(newStr);
+        System.out.println(newStr);      // Outcome
     }
     public static void main(String[] args) {
         String str = "aaabbccvvdd";
-          compressString(str) ;
+          compressString(str) ;          // passing str as an argument.
     }
 }
