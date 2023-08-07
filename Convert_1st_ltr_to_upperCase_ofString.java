2bace40eb6dbdf38caa0fc9836e973c4871f6bb9
HUZAIFA SHAHAB <huzaifarifatullah@gmail.com>
2023-08-07 15:40:57 +0500

    Create Convert_1st_ltr_to_upperCase_ofString.java

---
+++ Convert_1st_ltr_to_upperCase_ofString.java
@@ -0,0 +1,14 @@
+public class Convert_1st_ltr_to_upperCase_ofString {
+    public static void letterUppercase (String str){
+        StringBuilder newStr = new StringBuilder("") ;
+        int i=0 ;
+
+        for (i=1 ; i<str.length(); i++){
+           
+        }
+    }
+    public static void main(String[] args) {
+        String str = "hi, iam huzaifa shahab" ;
+            letterUppercase(str);
+    }
+}
