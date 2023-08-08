3f59709e5b7befd6d82a6162d407f68cff31a65d
HUZAIFA SHAHAB <huzaifarifatullah@gmail.com>
2023-08-08 15:37:31 +0500

    Update lowerCaseVowelsCounting.java

--- lowerCaseVowelsCounting.java
+++ lowerCaseVowelsCounting.java
@@ -1,7 +1,9 @@
 public class lowerCaseVowelsCounting {
     public static void main(String[] args) {
 
-        String str = "PAKISTANI" ;
+        String str = "Pakistani" ;
+        str.replace("i","o") ;
+        System.out.println(str);
         int counter = 0 ;
             for (int i=0 ; i<str.length() ;i++){
                 if (str.charAt(i)=='a'||str.charAt(i)=='i'||str.charAt(i)=='o'||str.charAt(i)=='e'||str.charAt(i)=='u'){
@@ -12,5 +14,6 @@ public class lowerCaseVowelsCounting {
                 System.out.println("LowerCase Vowels occur ("+counter+") times in a given string");
             else
                 System.out.println("No LowerCase Vowels in this case");
+
     }
 }
