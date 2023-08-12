5896e117e405c1d561be3f4ad5ef39695373bf5b
HUZAIFA SHAHAB <huzaifarifatullah@gmail.com>
2023-08-12 19:50:27 +0500

    Create Main.java

---
+++ out/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ArraySort arraySort = new ArraySort() ;
+        Scanner sc = new Scanner(System.in) ;
+
+        final int size = 5 ;
+        int []array = new int[size] ;
+
+        for (int i=0 ;i< array.length;i++){
+            System.out.print("Input Element No "+i+1+" : "+sc.nextInt());
+            System.out.println();
+        }
+
+        
+    }
+}
