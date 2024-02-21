3208cea2b7e75606aeda74d765ad30a4e670ea78
HUZAIFA SHAHAB <huzaifarifatullah@gmail.com>
2024-02-21 20:08:38 +0500

    Update countingNumbersIn2DArray.java

--- countingNumbersIn2DArray.java
+++ countingNumbersIn2DArray.java
@@ -1,5 +1,6 @@
 public class countingNumbersIn2DArray {
     //Huzaifa Shahab dummy-test 1
+    // practicing 2nd
     public static int duplicateNumbers(int[][] multiArray , int key){
 
         int count = 0 ;
